243834465b97f94c6c10b6397175e5d5afddc47a
rofifzm <zainulrofif@gmail.com>
2020-07-30 07:51:21 +0700

    commit kedua

---
+++ skor-terbesar.php
@@ -0,0 +1,72 @@
+<?php
+function skor_terbesar($arr){
+//kode di sini
+//buat array penyimpanan baru
+//tes array utk menyimpan per kelas
+//ambil dari variabel kelas
+//bila kelas sama bandingkan nilainya
+//ambil yang terbesar
+$hasil=[];
+$hasil_item=[];
+$p_array=count($arr);
+//echo $p_array;
+for ($i=0;$i<$p_array;$i++){
+    $hasil[]=$arr[$i]['kelas'];
+}
+return $hasil;
+
+}
+
+// TEST CASES
+$skor = [
+  [
+    "nama" => "Bobby",
+    "kelas" => "Laravel",
+    "nilai" => 78
+  ],
+  [
+    "nama" => "Regi",
+    "kelas" => "React Native",
+    "nilai" => 86
+  ],
+  [
+    "nama" => "Aghnat",
+    "kelas" => "Laravel",
+    "nilai" => 90
+  ],
+  [
+    "nama" => "Indra",
+    "kelas" => "React JS",
+    "nilai" => 85
+  ],
+  [
+    "nama" => "Yoga",
+    "kelas" => "React Native",
+    "nilai" => 77
+  ],
+];
+
+print_r(skor_terbesar($skor));
+/* OUTPUT
+  Array (
+    [Laravel] => Array
+              (
+                [nama] => Aghnat
+                [kelas] => Laravel
+                [nilai] => 90
+              )
+    [React Native] => Array
+                  (
+                    [nama] => Regi
+                    [kelas] => React Native
+                    [nilai] => 86
+                  )
+    [React JS] => Array
+                (
+                  [nama] => Indra
+                  [kelas] => React JS
+                  [nilai] => 85
+                )
+  )
+*/
+?>
